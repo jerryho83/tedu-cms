46fd6deb4c8fe2916e4330fb204438f42567e5da
ToanBN <tedu.international@gmail.com>
2016-02-11 19:34:02 +0700

    try call service

--- TEDU.Web/Areas/Admin/Controllers/HomeController.cs
+++ TEDU.Web/Areas/Admin/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TEDU.Service;
 
 namespace TEDU.Web.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICategoryService categoryService;
+        private readonly IPostService postService;
+        public HomeController(ICategoryService categoryService, IPostService postService)
+        {
+            this.categoryService = categoryService;
+            this.postService = postService;
+        }
         // GET: Admin/Home
         public ActionResult Index()
         {
