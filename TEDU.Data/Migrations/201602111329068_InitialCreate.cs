a2f06643f675438dbcc6352c6b89a2b86fc775cd
ToanBN <toan.bach-ngoc@tek-experts.com>
2016-02-23 11:23:40 +0700

    config seed data

--- TEDU.Data/Migrations/201602111329068_InitialCreate.cs
+++ TEDU.Data/Migrations/201602111329068_InitialCreate.cs
@@ -2,7 +2,7 @@ namespace TEDU.Data.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class InitialCreate : DbMigration
     {
         public override void Up()
@@ -10,47 +10,47 @@ namespace TEDU.Data.Migrations
             CreateTable(
                 "dbo.Categories",
                 c => new
-                    {
-                        ID = c.Int(nullable: false),
-                        Name = c.String(nullable: false),
-                        Alias = c.String(nullable: false),
-                        ParentID = c.Int(),
-                        CreatedDate = c.DateTime(nullable: false),
-                        CreatedBy = c.String(),
-                        LastModifiedBy = c.String(),
-                        LastModifiedDate = c.DateTime(nullable: false),
-                        Status = c.String(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                    Alias = c.String(nullable: false),
+                    ParentID = c.Int(),
+                    CreatedDate = c.DateTime(nullable: false),
+                    CreatedBy = c.String(),
+                    LastModifiedBy = c.String(),
+                    LastModifiedDate = c.DateTime(nullable: false),
+                    Status = c.String(nullable: false),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "dbo.Posts",
                 c => new
-                    {
-                        ID = c.Int(nullable: false),
-                        Name = c.String(nullable: false),
-                        Alias = c.String(nullable: false),
-                        Description = c.String(),
-                        CategoryID = c.Int(nullable: false),
-                        Image = c.String(),
-                        Content = c.String(),
-                        PostType = c.String(nullable: false),
-                        Source = c.String(),
-                        Status = c.String(nullable: false),
-                        ViewCount = c.Int(),
-                        Tags = c.String(),
-                        CreatedDate = c.DateTime(nullable: false),
-                        CreateBy = c.String(),
-                        LastModifiedDate = c.DateTime(nullable: false),
-                        LastModifiedBy = c.String(),
-                        OtherStatus = c.String(),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                    Alias = c.String(nullable: false),
+                    Description = c.String(),
+                    CategoryID = c.Int(nullable: false),
+                    Image = c.String(),
+                    Content = c.String(),
+                    PostType = c.String(nullable: false),
+                    Source = c.String(),
+                    Status = c.String(nullable: false),
+                    ViewCount = c.Int(),
+                    Tags = c.String(),
+                    CreatedDate = c.DateTime(nullable: false),
+                    CreateBy = c.String(),
+                    LastModifiedDate = c.DateTime(nullable: false),
+                    LastModifiedBy = c.String(),
+                    OtherStatus = c.String(),
+                })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
                 .Index(t => t.CategoryID);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.Posts", "CategoryID", "dbo.Categories");
--- TEDU.Data/Migrations/Configuration.cs
+++ TEDU.Data/Migrations/Configuration.cs
@@ -26,21 +26,18 @@ namespace TEDU.Data.Migrations
             return new List<Category>
             {
                 new Category {
-                    ID=1,
                     Name = ".NET",
                     Alias = "dotnet",
                     Status = StatusEnum.Publish.ToString(),
                     CreatedDate=DateTime.Now
                 },
                 new Category {
-                    ID= 2,
                     Name = "Database",
                     Alias = "database",
                     Status = StatusEnum.Publish.ToString(),
                      CreatedDate=DateTime.Now
                 },
                new Category {
-                   ID=3,
                     Name = "Front End",
                     Alias = "front-end",
                     Status = StatusEnum.Publish.ToString(),
@@ -54,7 +51,6 @@ namespace TEDU.Data.Migrations
             return new List<Post>
             {
                 new Post {
-                    ID=1,
                     Name = "ProntoTec 7",
                     Description = "Android 4.4 KitKat Tablet PC, Cortex A8 1.2 GHz Dual Core Processor,512MB / 4GB,Dual Camera,G-Sensor (Black)",
                     CategoryID = 1,
@@ -65,7 +61,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=2,
                     Name = "Samsung Galaxy",
                     Description = "Android 4.4 Kit Kat OS, 1.2 GHz quad-core processor",
                     CategoryID = 1,
@@ -76,7 +71,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=3,
                     Name = "NeuTab® N7 Pro 7",
                     Description = "NeuTab N7 Pro tablet features the amazing powerful, Quad Core processor performs approximately Double multitasking running speed, and is more reliable than ever",
                     CategoryID = 1,
@@ -87,7 +81,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=4,
                     Name = "Dragon Touch® Y88X 7",
                     Description = "Dragon Touch Y88X tablet featuring the incredible powerful Allwinner Quad Core A33, up to four times faster CPU, ensures faster multitasking speed than ever. With the super-portable size, you get a robust power in a device that can be taken everywhere",
                     CategoryID = 1,
@@ -97,7 +90,6 @@ namespace TEDU.Data.Migrations
                     Status = StatusEnum.Publish.ToString(),
                 },
                 new Post {
-                    ID=5,
                     Name = "Alldaymall A88X 7",
                     Description = "This Alldaymall tablet featuring the incredible powerful Allwinner Quad Core A33, up to four times faster CPU, ensures faster multitasking speed than ever. With the super-portable size, you get a robust power in a device that can be taken everywhere",
                     CategoryID = 1,
@@ -108,7 +100,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=6,
                     Name = "ASUS MeMO",
                     Description = "Pad 7 ME170CX-A1-BK 7-Inch 16GB Tablet. Dual-Core Intel Atom Z2520 1.2GHz CPU",
                     CategoryID = 1,
@@ -119,7 +110,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=7,
                     Name = "ASUS 15.6-Inch",
                     Description = "Latest Generation Intel Dual Core Celeron 2.16 GHz Processor (turbo to 2.41 GHz)",
                     CategoryID = 2,
@@ -130,7 +120,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=8,
                     Name = "HP Pavilion 15-r030wm",
                     Description = "This Certified Refurbished product is manufacturer refurbished, shows limited or no wear, and includes all original accessories plus a 90-day warranty",
                     CategoryID = 2,
@@ -141,7 +130,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=9,
                     Name = "Dell Inspiron 15.6-Inch",
                     Description = "Intel Celeron N2830 Processor, 15.6-Inch Screen, Intel HD Graphics",
                     CategoryID = 2,
@@ -152,7 +140,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=10,
                     Name = "Acer Aspire E Notebook",
                     Description = "15.6 HD Active Matrix TFT Color LED (1366 x 768) 16:9 CineCrystal Display",
                     CategoryID = 2,
@@ -163,7 +150,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=11,
                     Name = "HP Stream 13",
                     Description = "Intel Celeron N2840 Processor. 2 GB DDR3L SDRAM, 32 GB Solid-State Drive and 1TB OneDrive Cloud Storage for one year",
                     CategoryID = 2,
@@ -174,7 +160,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=12,
                     Name = "Nokia Lumia 521",
                     Description = "T-Mobile Cell Phone 4G - White. 5MP Camera - Snap creative photos with built-in digital lenses",
                     CategoryID = 3,
@@ -185,7 +170,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=13,
                     Name = "HTC Desire 816",
                     Description = "13 MP Rear Facing BSI Camera / 5 MP Front Facing",
                     CategoryID = 3,
@@ -196,7 +180,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=14,
                     Name = "Sanyo Innuendo",
                     Description = "Uniquely designed 3G-enabled messaging phone with side-flipping QWERTY keyboard and external glow-thru OLED dial pad that 'disappears' when not in use",
                     CategoryID = 3,
@@ -207,7 +190,6 @@ namespace TEDU.Data.Migrations
                      CreatedDate=DateTime.Now
                 },
                 new Post {
-                    ID=15,
                     Name = "Ulefone N9000",
                     Alias = "ulefont-n9000",
                     PostType = "news",
