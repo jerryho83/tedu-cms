69d022386742421d25088c4b1cbaf64c04498831
ToanBN <tedu.international@gmail.com>
2016-04-22 07:46:38 +0700

    change list category

---
+++ TEDU.Web/app/layout/treeGrid.directive.js
@@ -0,0 +1,25 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('common.ui')
+        .directive('treeGrid', treeGrid);
+    function treeGrid () {
+        // Usage:
+        //     <treeGrid></treeGrid>
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'E'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            angular.element(document).ready(function () {
+                $(element).treegrid();
+            });
+        }
+    }
+
+})();
