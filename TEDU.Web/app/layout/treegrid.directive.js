a509e01d3dafbbe0e081fb8807412a2ce1736393
Bach Ngoc Toan <ngoctoan@live.com>
2016-04-22 10:53:26 +0700

    comit plugins

---
+++ TEDU.Web/app/layout/treegrid.directive.js
@@ -0,0 +1,17 @@
+﻿(function (app) {
+    'use strict';
+    app.directive('treegrid', treegrid);
+
+    function treegrid() {
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+           $(element).treegrid();
+        }
+    }
+
+})(angular.module('common.ui'));
---
+++ TEDU.Web/scripts/plugins/angular-validators/dist/equality.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    angular.module('angularValidators')
+
+        .directive('ivEquals', function() {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function (scope, elem, attrs, ngModel)
+                {
+                    if (!ngModel) {
+                        return;
+                    }
+
+                    attrs.$observe('ivEquals', function() {
+                        ngModel.$validate();
+                    });
+
+                    ngModel.$validators.equals = function(value)
+                    {
+                        return value === attrs.ivEquals;
+                    };
+                }
+            };
+        })
+
+        .directive('ivNotEquals', function() {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function (scope, elem, attrs, ngModel)
+                {
+                    if (!ngModel) {
+                        return;
+                    }
+
+                    attrs.$observe('ivNotEquals', function() {
+                        ngModel.$validate();
+                    });
+
+                    ngModel.$validators.notEquals = function(value)
+                    {
+                        return value !== attrs.ivNotEquals;
+                    };
+                }
+            };
+        });
+        
+}());
