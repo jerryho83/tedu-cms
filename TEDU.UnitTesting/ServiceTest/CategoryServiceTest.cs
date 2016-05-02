702564d79ba8e4b2e1fb9db3e40f948a8a3fcdb0
ToanBN <tedu.international@gmail.com>
2016-05-02 08:34:22 +0700

    move venders

---
+++ TEDU.UnitTesting/ServiceTest/CategoryServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using TEDU.Data.Infrastructure;
+using TEDU.Data.Repositories;
+using TEDU.Model;
+using TEDU.Service;
+
+namespace TEDU.UnitTesting.ServiceTest
+{
+    [TestClass]
+    public class CategoryServiceTest
+    {
+        private Mock<ICategoryRepository> _mockRepository;
+        private ICategoryService _service;
+        private Mock<IUnitOfWork> _mockUnitWork;
+        private List<Category> _listCategory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<ICategoryRepository>();
+            _mockUnitWork = new Mock<IUnitOfWork>();
+            _service = new CategoryService(_mockRepository.Object, _mockUnitWork.Object);
+            _listCategory = new List<Category>() {
+               new Category() { ID = 1, Name = "US" },
+               new Category() { ID = 2, Name = "India" },
+               new Category() { ID = 3, Name = "Russia" }
+          };
+        }
+
+        [TestMethod]
+        public void Category_Get_All()
+        {
+            //Arrange
+            _mockRepository.Setup(x => x.GetMany(c => c.Status)).Returns(_listCategory);
+
+            //Act
+            List<Category> results = _service.GetCategories() as List<Category>;
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(3, results.Count);
+        }
+
+
+    }
+}
