58de2b1bfe5b18db0df5ad33e8b0bfcf4e785851
ToanBN <tedu.international@gmail.com>
2016-05-09 21:34:12 +0700

    fix bug the first redirect

--- TEDU.Web/Areas/Admin/Controllers/HomeController.cs
+++ TEDU.Web/Areas/Admin/Controllers/HomeController.cs
@@ -3,16 +3,10 @@ using TEDU.Service;
 
 namespace TEDU.Web.Areas.Admin.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ICategoryService _categoryService;
-        private readonly IPostService _postService;
-
-        public HomeController(ICategoryService categoryService, IPostService postService)
+        public HomeController()
         {
-            this._categoryService = categoryService;
-            this._postService = postService;
         }
 
         // GET: Admin/Home
